cdcf3e041f46bfe7721c3dab5d608b560df066aa
RangiraDave <rangiradev666@gmail.com>
2023-02-16 11:43:18 -0800

    compiled 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-/*main function for our puts program*/
-int main(void)
+int main(void)/*main funtion for our program*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
