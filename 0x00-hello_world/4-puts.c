0b02ec369f3168d30b0e2bc8048a4372711a3740
RangiraDave <rangiradev666@gmail.com>
2023-02-16 12:03:12 -0800

    Corrected some errors

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main(void)/*main funtion for our program*/
+int main(void) /*main funtion for our program*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
