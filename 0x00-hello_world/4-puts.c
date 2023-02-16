ac2392cecbf857b433c36d4fdce8888bddd3a8b3
RangiraDave <rangiradev666@gmail.com>
2023-02-16 12:11:49 -0800

    Adding 5-pritf Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(void) /*main funtion for our program*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
