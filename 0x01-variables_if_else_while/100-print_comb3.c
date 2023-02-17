f18b131caa6b3f5adae91d815b99e6dc56b4cf37
RangiraDave <rangiradev666@gmail.com>
2023-02-17 12:38:36 -0800

    two combinations of digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			if (i != j)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+			}
+			if (i == 8 && j == 9)
+				continue;
+			if (i < 9)
+			{
+				putchar(',');
+				putchar(',');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
