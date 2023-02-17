ee6da4ec37468f69bb428d82dcdd2900c936b4e3
RangiraDave <rangiradev666@gmail.com>
2023-02-17 10:31:32 -0800

    Adding 3-print_alphabets in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - start point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char Ch;
+	char Ch1;
+
+	for (Ch == 'A'; Ch <= 'Z'; Ch++)
+	{
+		Ch1=tolower(Ch);
+		putchar(Ch);
+		putchar(Ch1);
+	}
+	putchar('\n');
+	return (0);
+}
