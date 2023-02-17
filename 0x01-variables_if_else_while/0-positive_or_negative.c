1c561efc9c4c0f7f55127a6c0ea4c56fb63162f2
RangiraDave <rangiradev666@gmail.com>
2023-02-17 07:07:29 -0800

    Added a header in 0-.. task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,24 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
 /* betty style doc for function main goes there */
-int main(void)
+int main()
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n == 0){
-		ptintf("%d", n, "is zero");
+		printf ("%i", n);
+		printf (" is zero\n");
 	}
-	else if(n > 0){
-		printf("%d", n, "is positive");
+	else if (n > 0){
+		printf ("%i", n);
+		printf (" is positive\n");
 	}
 	else{
-		printf("%d", n, "is negative");
+		printf ("%i", n);
+		printf (" is negative\n");
 	}
 	return (0);
 }
