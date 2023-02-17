a9fcbc61455ca18453fc20673691fc44ca9c8267
RangiraDave <rangiradev666@gmail.com>
2023-02-17 11:28:10 -0800

    Displaying numbers by using putchar

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * Where main starts
+ * main - start point
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -8,7 +8,7 @@ int main(void)
 	int a;
 
 	for (a = 0; a < 10; a++)
-		printf("%d",a);
+		printf("%d", a);
 	printf("\n");
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0'+i);
+	}
+	putchar('\n');
+	return (0);
+}
