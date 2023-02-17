42285281c6ce4d70a72b1559b83004b687706039
RangiraDave <rangiradev666@gmail.com>
2023-02-17 07:14:57 -0800

    Removed " (" => "("

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,23 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
+
 /* betty style doc for function main goes there */
-int main()
+int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n == 0){
-		printf ("%i", n);
-		printf (" is zero\n");
+	if (n == 0)
+	{
+		printf("%i", n);
+		printf(" is zero\n");
 	}
-	else if (n > 0){
-		printf ("%i", n);
-		printf (" is positive\n");
+	else if (n > 0)
+	{
+		printf("%i", n);
+		printf(" is positive\n");
 	}
-	else{
-		printf ("%i", n);
-		printf (" is negative\n");
+	else
+	{
+		printf("%i", n);
+		printf(" is negative\n");
 	}
 	return (0);
 }
