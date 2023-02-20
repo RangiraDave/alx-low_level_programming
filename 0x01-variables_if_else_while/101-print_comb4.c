718132020e933de99d082d82df37dc4a33897df3
RangiraDave <rangiradev666@gmail.com>
2023-02-20 07:00:08 -0800

    Corrected 101-print_com4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,33 +5,26 @@
  */
 int main(void)
 {
-        int i;
-        int j;
+	int i;
+	int j;
 	int k;
 
-        for (i = 0; i <= 9; i++)
-        {
-                for (j = i + 1; j <= 9; j++)
-                {
-			for (k = j + 1; k <= j; k++)
+	for (i = 0; i< 8; i++)
+	{
+		for (j = i + 1; j < 9; j++)
+		{
+			for (k = j + 1; k < 10; k++)
 			{
-                        	if (i != j)
-                        	{
-                                	putchar('0' + i);
-					putchar('0' + j);
-					putchar('0' + k);
-                        	}
-                        	if (i == 7 && j == 8 && k == 9)
-                               		continue;
-                        	if (i < 9)
-                        	{
-                                	putchar(',');
-                                	putchar(' ');
-                        	}
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + k);
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
 			}
-                }
-        }
-        putchar('\n');
-        return (0);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
-
