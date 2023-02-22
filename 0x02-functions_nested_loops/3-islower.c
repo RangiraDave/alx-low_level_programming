e942987ed75402c2c07f89a4e00ee86908f77a05
RangiraDave <rangiradev666@gmail.com>
2023-02-22 01:26:23 -0800

    adding islower function

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,5 @@
-#include "stdio.h"
-#include <ctype.h>
 /**
- * main - check the code.
+ * _islower - check the code.
  * Return: Always 0.
  */
 char _islower(char n)
@@ -10,16 +8,4 @@ char _islower(char n)
 		return (1);
 	else
 		return (0);
-}
-int main(void)
-{
-	char r;
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
 }
