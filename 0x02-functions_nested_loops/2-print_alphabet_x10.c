f8341e61a5e33cda3f7125015c62680d730eeb04
RangiraDave <rangiradev666@gmail.com>
2023-02-22 00:22:13 -0800

    added void in 2 s function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,12 +3,11 @@
  * print - check the code.
  * Return: Always 0 (Success)
  */
-void print_alphabet()
+void print_alphabet(void)
 {
 	char i;
 
 	for (i = 'a'; i <= 'z'; i++)
 		_putchar(i);
-
 	_putchar('\n');
 }
