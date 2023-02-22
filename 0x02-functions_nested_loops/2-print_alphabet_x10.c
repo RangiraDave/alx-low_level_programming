3e430a41e67e43081633bba4211862dd390500eb
RangiraDave <rangiradev666@gmail.com>
2023-02-22 01:11:57 -0800

    Corrected inside of function function

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,8 +6,10 @@
 void print_alphabet_x10(void)
 {
 	char i;
+	int n;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+	for (n = 0; n <= 9; n++)
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
 }
