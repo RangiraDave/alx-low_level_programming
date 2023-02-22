5a3cf3232d226def7b379c119a3cbfff95c7bf46
RangiraDave <rangiradev666@gmail.com>
2023-02-22 11:00:41 -0800

    Adding multiples of 3 or 5 under 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i <= 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 ==0))
+		{
+			sum += i;
+		}
+	}
+	printf("%i", sum);
+	return (0);
+}
