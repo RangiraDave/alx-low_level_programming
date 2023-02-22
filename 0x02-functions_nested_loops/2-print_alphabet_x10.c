b21af0205005051cfebf79055bbf724b005a5428
RangiraDave <rangiradev666@gmail.com>
2023-02-22 01:18:49 -0800

    guarded for ( {} )

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print - check the code.
+ * print_alphabet - check the code
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
@@ -9,7 +9,11 @@ void print_alphabet_x10(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
+	{
 		for (i = 'a'; i <= 'z'; i++)
+		{
 			_putchar(i);
+		}
 		_putchar('\n');
+	}
 }
