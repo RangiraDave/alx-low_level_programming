fe80335e8bd122b3dfd1cb7a34ab8ac37e472b15
RangiraDave <rangiradev666@gmail.com>
2023-02-23 06:27:36 -0800

    Removed spacebars and placed there tabs

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,25 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest;
 
-    if (a > b && a > c)
-    {
-        largest = a;
-    }
-    else if (b > a && b > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-  return (largest);
+	if (a < b || a < c)
+	{
+	    if (b > c)
+		    largest = b;
+	    else
+		    largest = c;
+	}
+	else if (b < a || b < c)
+	{
+	    if (a > c)
+		    largest = a;
+	    else
+		    largest = c;
+	}
+	else
+	{
+		largest = c;
+	}
+	return (largest);
 }
