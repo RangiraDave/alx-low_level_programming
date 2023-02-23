e53a3595a9bbe97d38591d80890a68be02923430
RangiraDave <rangiradev666@gmail.com>
2023-02-23 10:04:31 -0800

    Corrected discribtion

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 /**
  * _isdigit - Check for digits (0 through 9)
- * @c: int type param
+ * @c: Only variable
  * Return: 1 if digit, else 0
  */
 int _isdigit(int c)
 {
-	if (c > 13 && c < 24)
+	if (c > 47 && c < 58)
 		return (1);
-
 	return (0);
 }
