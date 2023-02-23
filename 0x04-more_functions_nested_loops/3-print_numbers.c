0a8279cf8c88b5c4d9bed03176f4ef3a203fc625
RangiraDave <rangiradev666@gmail.com>
2023-02-23 10:38:12 -0800

    printing from 0 to 9 but not 2 and 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,7 +7,8 @@
 void print_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
-	       _putchar(i);
+		_putchar(i + '0');
 	_putchar('\n');
 }
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - from 0 to 9 but not 2 and 4
+ * @i: Variable for increamantation
+ * Return: Always 0 (Success)
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
