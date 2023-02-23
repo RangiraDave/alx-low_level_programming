2985cd4862551493cb676d98c6fd8363e27fd953
RangiraDave <rangiradev666@gmail.com>
2023-02-23 13:01:28 -0800

    Printing squeres of #

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,20 +1,24 @@
 #include "main.h"
 /**
  * print_square - to print square if #
+ * Description: printing
+ *
  */
 void print_square(int size)
 {
-	int i, j;
-	
-	for (i = 1; i <= size; i++)
-	{
-		for (j = 1; j <= size; j++)
-		{
-			_putchar('#');
-		}
-	
-		_putchar('\n');
-	}
 	if (size <= 0)
 		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
