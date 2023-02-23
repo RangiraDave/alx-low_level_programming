67dee1742de2585d612aff204fc7fcd051917bef
RangiraDave <rangiradev666@gmail.com>
2023-02-23 13:24:26 -0800

    fizz bazz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - checks the codes
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf(" Fizz");
+		else if (i % 5 == 0)
+			printf(" Buzz");
+		else if (i > 1)
+			printf(" %i", i);
+		else
+			printf("%i", i);
+	}
+	printf("\n");
+}
