04007ae33537b76f0d6db0c01db4f62fe8c35d8b
RangiraDave <rangiradev666@gmail.com>
2023-02-24 12:55:24 -0800

    Largest of the three

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,25 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a < b || a < c)
-	{
-	    if (b > c)
-		    largest = b;
-	    else
-		    largest = c;
-	}
-	else if (b < a || b < c)
-	{
-	    if (a > c)
-		    largest = a;
-	    else
-		    largest = c;
-	}
-	else
-	{
-		largest = c;
-	}
-	return (largest);
+    if (a >= b && b >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && a >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
 }
