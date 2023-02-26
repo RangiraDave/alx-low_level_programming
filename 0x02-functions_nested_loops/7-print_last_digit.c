612770e088997c4102958c8e1dceea3fac3e9247
RangiraDave <rangiradev666@gmail.com>
2023-02-26 12:27:28 -0800

    Displaying the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
  * print_last_digit - look for last digit
- * @d: variable
+ * @n: variable
  * Return: Always 0
  */
 int print_last_digit(int n)
 {
 	int m;
 
+	if (n == 0)
+		return (0);
 	while (n != 0)
 	{
-		n = n / 10;
-		m = n * 10;
+		n = (n / 10);
+		m = (n * 10);
 	}
 	return (m);
 }
