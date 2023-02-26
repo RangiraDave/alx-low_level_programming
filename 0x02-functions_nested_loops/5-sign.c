9de56888bb8748d167f56117f6947de11313000e
RangiraDave <rangiradev666@gmail.com>
2023-02-26 11:33:49 -0800

    used _putchar instead of return

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,16 +12,15 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('1' + '0');
+
 	}
 	else if (n == 0)
 	{
 		_putchar('0' + '0');
-		_putchar('0' + '0');
+
 	}
 	else
 	{
 			_putchar('-');
-			_putchar('1' + '0');
 	}
 }
