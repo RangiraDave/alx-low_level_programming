d355ac976336bd733f8b7aa8ab8828c676dbff1e
RangiraDave <rangiradev666@gmail.com>
2023-02-27 01:47:23 -0800

    First pointer array and strings Task

---
+++ 0x05-pointers_array_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - to change the value of a variable
+ * @n: A pointer to the variable
+ * Return: Always 0 (Success)
+ */
+void reset_to_98(int *n)
+{
+	int t;
+
+	*n = t;
+	return (0);
+}
