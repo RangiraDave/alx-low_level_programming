0781ac3b3547def879832de2cec96cc5da32a555
RangiraDave <rangiradev666@gmail.com>
2023-02-27 12:41:48 -0800

    displaying elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - Displaying elements of an array
+ * @a: pointer
+ * @n: number of elements
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
+}
