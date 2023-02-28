7a973b2442ec1a80c4ea3b2da6d9ce650747080a
RangiraDave <rangiradev666@gmail.com>
2023-02-28 11:24:34 -0800

    Converting str into int

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <limits.h>
 /**
  * _atoi - converts string to integer
  * @s: pointer to string
@@ -7,13 +8,11 @@
 
 int _atoi(char *s)
 {
-	int sign = 1;   // sign as positive
-	int result = 0; // result as 0
+	int sign = 1;
+	int result = 0;
 
-	// Skip white spaces
 	while (*s == ' ')
 		s++;
-	// count signs
 	if (*s == '-')
 	{
 		sign = -1;
@@ -23,18 +22,13 @@ int _atoi(char *s)
 	{
 		s++;
 	}
-	// Convert digits to intege	
 	while (*s >= '0' && *s <= '9')
 	{
-		// Check for overflow
 		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && *s - '0' > INT_MAX % 10))
 		{
-			// Overflow occurred
 			return sign == 1 ? INT_MAX : INT_MIN;
 		}
-		// Update result
 		result = result * 10 + (*s - '0');
-		// Move to the next character
 		s++;
 	}
 	return (sign * result);
