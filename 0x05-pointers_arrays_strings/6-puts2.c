0a0d7318b22c569ce578c024c63bad95f990214b
RangiraDave <rangiradev666@gmail.com>
2023-02-27 23:56:59 -0800

    To print some characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every other char of string
+ * @str: Pointer
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	j = 0;
+	while (*str)
+	{
+		*str++;
+		j++;
+	}
+	for (i = 0; i <= j; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(*str);
+	}
+}
