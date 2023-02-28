cf6464f97558efe6099b3d498d0497ac2cb8c395
RangiraDave <rangiradev666@gmail.com>
2023-02-28 07:26:17 -0800

    started second loop at n/2

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,12 +16,12 @@ void puts_half(char *str)
 	if (n % 2 == 0)
 	{
 		for (i = 0; i <= n / 2; i++)
-			_putchar(*str++);
+			_putchar(*(str - n / 2 + i));
 	}
 	else
 	{
 		for (i = (n - 1) / 2 + 1; i < n; i++)
-			_putchar(*str++);
+			_putchar(*(str - n / 2 + i));
 	}
 	_putchar('\n');
 }
