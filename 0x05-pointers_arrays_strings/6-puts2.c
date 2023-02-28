a0eaeae3f6d944a3e2f4e406584edcd65edc3e9f
RangiraDave <rangiradev666@gmail.com>
2023-02-28 00:40:15 -0800

    Added prototype in main.h function and remooved for loop

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,13 @@ void puts2(char *str)
 	int j;
 
 	j = 0;
-	while (*str)
+	while (*str++)
 	{
 		if (j % 2 == 0)
-			_putchar(*str++);
+			_putchar(*str);
 		else
 			continue;
 		j++;
 	}
+	_putchar('\n');
 }
