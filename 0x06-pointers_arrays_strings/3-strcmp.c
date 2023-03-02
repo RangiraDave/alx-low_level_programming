c24930e49f01d605a919c10dbe262021a9870387
RangiraDave <rangiradev666@gmail.com>
2023-03-02 00:23:10 -0800

    Function like strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * strcmp - Compares two strings
+ * @s1: pointer to first string
+ * @s2: Points to second string
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] && s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+	}
+	if (s1[i] == '\0')
+	{
+		return (-s2[i]);
+	}
+	else if (s2[i] == '\0')
+		return (s1[i]);
+	else
+		return (0);
+}
