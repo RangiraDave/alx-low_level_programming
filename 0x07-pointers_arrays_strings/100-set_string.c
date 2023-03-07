3f8ad1cfdae1d6c92f8465342a5191c09443e8e2
RangiraDave <rangiradev666@gmail.com>
2023-03-07 10:20:33 -0800

    Function thst sets a pointer to char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * set_string - Puts on string into an other variable
+ * @s: Pointer to pointer to string
+ * @to: Pointer to string
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,3 +6,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
+void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
