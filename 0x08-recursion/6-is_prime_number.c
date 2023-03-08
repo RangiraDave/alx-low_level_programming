f61581c2014def075b6e1d8671b01fcb51afc97f
RangiraDave <rangiradev666@gmail.com>
2023-03-08 13:07:24 -0800

    added discription for is_prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,12 @@
  */
 int is_prime_number(int n)
 {
-	if (n % 1 == 0 && n % n == 0)
+	if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (0);
+	else if (n % 1 == 0 && n % n == 0)
 		return (1);
-	return (0);
+	else
+		return (0);
 }
