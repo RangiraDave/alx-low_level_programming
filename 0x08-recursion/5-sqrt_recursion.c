95cea058cd6d8ea00e669c7d862da9a1cc580a59
RangiraDave <rangiradave6@gmail.com>
2023-08-02 11:51:27 +0200

    Adding solutions

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _pow_recursion - give the power
+ * @n: input
+ * Return: The natural number of input & if not, -1.
+ */
+
+int _sqrt_recursion(int n){
+	i = n;
+	if (i * i == n){
+		return i;
+	}
+}
