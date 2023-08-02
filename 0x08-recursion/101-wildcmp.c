08089183c5725f6763aa855246a1fdd7ff4814f5
RangiraDave <rangiradave6@gmail.com>
2023-08-02 15:57:52 +0200

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int wildcmp - prints 1 if identical & 0 otherwise
+ * wildcmp - prints 1 if identical & 0 otherwise
  * @s1: First string
  * @s2: Second string
  * Return: 1 or 0
@@ -8,9 +8,10 @@
 int wildcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 	if (s1[i] == s2[i])
 		return (1);
 	else
 		return (0);
-	return (wildcmp(s1 + 1, s2 + 1));
+	return wildcmp(s1 + 1, s2 + 1);
 }
