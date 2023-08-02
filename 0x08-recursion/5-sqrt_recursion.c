b22f4307bfcab34e303c946edab7e63a696dbd27
RangiraDave <rangiradave6@gmail.com>
2023-08-02 14:57:29 +0200

    added () on return

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,20 +1,27 @@
 #include "main.h"
 /**
- * _pow_recursion - give the power
+ * sqroot - Checks for square root
+ * @i: To be compared to base
+ * @j: Base number
+ * _sqrt_recursion - Returns the chossen sqrt
  * @n: input
  * Return: The natural number of input & if not, -1.
  */
 
-int sqroot(int i, int j){
-	if (i * i == j){
+int sqroot(int i, int j)
+{
+	if (i * i == j)
+	{
 		return (i);
 	}
-	else{
+	else
+	{
 		return (-1);
 	}
 	return sqroot(i + 1, j);
 }
 
-int _sqrt_recursion(int n){
+int _sqrt_recursion(int n)
+{
 	return (sqroot(1, n));
 }
