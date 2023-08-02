e7234b6d013e0793808dd130b1299e1d326244c5
RangiraDave <rangiradave6@gmail.com>
2023-08-02 16:22:42 +0200

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -7,12 +7,11 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	if (s1[i] == s2[i])
+	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
-	else
-		return (0);
-	i++;
-	return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == "*")
+		return (wildcmp(s1, s2 + 1) ||(*s1 != '\0' && wildcmp(s1 + 1, s2)));
+return ('\0')
 }
