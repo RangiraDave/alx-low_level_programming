801c7c1d11685a1c3ca0553cfaffefef464499cb
RangiraDave <rangiradave6@gmail.com>
2023-08-04 08:50:17 +0200

    Displaying program name using argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - Root function
+ * @argc: Counter
+ * @argv: Array of strings
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = 0;
+	printf("%s\n", argv[argc]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+The folder containing argc and argv program
