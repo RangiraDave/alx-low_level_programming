42b843fffe08941e8d59a476dfbe8a6d50aa4ae3
RangiraDave <rangiradave6@gmail.com>
2023-08-06 09:24:58 +0200

    Changer

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<ctype.h>
-
 /**
  * main - Main function
  * @argc: Counter
  * @argv: Pointer
  * Return: Number of minimum coins for change
  */
-
 int main (int argc, char *argv[])
 {
 	int i;
@@ -16,12 +14,12 @@ int main (int argc, char *argv[])
 	int N_cents = 0;
 	int amount;
 
-	if (argc != 2){
+	if (argc != 2)
+	{
 		printf("Error\n");
 		return (1);
 	}
 	amount = atoi(argv[1]);
-
 	if (amount < 0)
 		printf("0\n");
 	else
