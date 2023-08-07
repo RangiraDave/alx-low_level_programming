1322b1959fcfff58afaad5ec9a833ccaf1032e59
RangiraDave <rangiradave6@gmail.com>
2023-08-07 16:52:26 +0200

    Duplicated string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+
+/**
+ * _strdup - Returns newly created string(duplicated)
+ * @str: pointer to new string
+ * Return: Duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	if (str == NULL)
+		return (NULL);
+
+	s = malloc(sizeof(str));
+
+	s = str;
+	if (s == NULL)
+		return (NULL);
+	return (s);
+	free (s);
+}
