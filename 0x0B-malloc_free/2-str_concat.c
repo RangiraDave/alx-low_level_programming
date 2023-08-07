65e42cbb71c9623368087a4fcdae62792750b88b
RangiraDave <rangiradave6@gmail.com>
2023-08-07 21:09:38 +0200

    Enterchanged loops

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,29 +10,27 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *sum;
-	long unsigned int len1, len2, i = 0;
+	long unsigned int len1 = 0, len2 = 0, i = 0;
 	
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[i] != '\0')
+	while (s1[len1] != '\0')
 	{
-		i++;
-		len1 = i;
+		len1++;
 	}
-	while (s2[i] != '\0')
+	while (s2[len2] != '\0')
 	{
-		i++;
-		len2 = i;
+		len2++;
 	}
+	sum = malloc((len1 + len2) * sizeof(char));
 	if (s1 == NULL)
 		sum = s1;
 	if (s2 == NULL)
 		sum = s2;
 	if (s1 == NULL && s2 == NULL)
 		sum = "";
-	sum = malloc((len1 + len2) * sizeof(char));
 	for (i = 0; i < len1; i++)
 		sum[i] = s1[i];
 	for (i = 0; i < len1 + len2; i++)
