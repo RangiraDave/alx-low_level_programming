bea5ffd3f2399371dc308181d0f739617d392fa6
RangiraDave <rangiradave6@gmail.com>
2023-08-07 22:33:01 +0200

    Allocatttted memory

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,24 +10,24 @@ int **alloc_grid(int width, int height)
 	int **Arr;
 	int i, j, k;
 
-	if(width <= 0 || height <= 0)
-		return(NULL);
-	Arr = malloc(width * sizeof(int*));
-	if(Arr == NULL)
-		return(NULL);
-	for(i = 0; i < height; i++)
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	Arr = malloc(width * sizeof(int));
+	if (Arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
 	{
 		Arr[i] = malloc(width * sizeof(int));
-		if(Arr[i] == NULL)
+		if (Arr[i] == NULL)
 		{
-			for(k = 0; k < height; k++)
+			for (k = 0; k < height; k++)
 				free(Arr[k]);
 			free(Arr);
-			return(NULL);
+			return (NULL);
 		}
 	}
-	for(i = 0; i < height; i++)
-		for(j = 0; j < width; j++)
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
 			Arr[i][j] = 0;
 	return (Arr);
 }
