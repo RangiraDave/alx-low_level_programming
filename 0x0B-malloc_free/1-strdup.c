c0aea6e54bd7742d9bed8b8bf46201a0d6f19580
RangiraDave <rangiradave6@gmail.com>
2023-08-08 09:09:30 +0200

    added 1 on the size of s

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 		return (NULL);
 	while (str[len] != '\0')
 		len++;
-	s = malloc((sizeof(len) + 1) * sizeof(char));
+	s = malloc((len + 1) * sizeof(char));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len; i++)
