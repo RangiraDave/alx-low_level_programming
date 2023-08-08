e75a1e3e17992404a7de908cd49a4427d163c05f
RangiraDave <rangiradave6@gmail.com>
2023-08-08 09:40:37 +0200

    Freeing allocated size for grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+int **alloc_grid(int width, int height)
+{
+        int **Arr;
+        int i, j, k;
+
+        if (width <= 0 || height <= 0)
+                return (NULL);
+        Arr = malloc(width * sizeof(int));
+        if (Arr == NULL)
+                return (NULL);
+        for (i = 0; i < height; i++)
+        {
+                Arr[i] = malloc(width * sizeof(int));
+                if (Arr[i] == NULL)
+                {
+                        for (k = 0; k < height; k++)
+                                free(Arr[k]);
+                        free(Arr);
+                        return (NULL);
+                }
+        }
+        for (i = 0; i < height; i++)
+                for (j = 0; j < width; j++)
+                        Arr[i][j] = 0;
+        return (Arr);
+}
