562ac1c4c40ef848708c6085cfa9aa258d2eb26c
RangiraDave <rangiradave6@gmail.com>
2023-08-10 21:24:16 +0200

    Pointer to array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+/**
+ * _calloc - Allocates memory
+ * @nmemb: Pointer to an int
+ * @size: Pointer to int
+ * Return: Pointer to an array
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+  char *p;
+  unsigned int ct;
+
+  if (nmemb == 0 || size == 0)
+    return (NULL);
+  p = malloc(nmemb * size);
+  if (p == NULL)
+    return (NULL);
+  ct = 0;
+  while (ct < nmemb * size)
+    {
+      p[ct] = 0;
+      ct++;
+    }
+  return (p);
+}
