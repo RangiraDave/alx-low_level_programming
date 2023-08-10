5d9e0c146ec56739f2caa19b9042d55783972688
RangiraDave <rangiradave6@gmail.com>
2023-08-10 21:41:36 +0200

    The range of an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include <string.h>
+/**
+ * array_range - make memory for an array
+ * @min: int type
+ * @max: int type
+ * Return: return pointer to array
+ */
+int *array_range(int min, int max)
+{
+      int *ptr;
+      int arr;
+
+      arr = 0;
+
+      if (min > max)
+	      return (NULL);
+      ptr = malloc(sizeof(int) * (max - min + 1));
+      if (ptr == NULL)
+	      return (NULL);
+      while (min <= max)
+      {
+	      ptr[arr] = min;
+	      min++;
+	      arr++;
+      }
+      return (ptr);
+}
