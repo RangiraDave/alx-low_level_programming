c411f100e1a0fb9a4edd8c3eb2c07bbbf6e37fd3
RangiraDave <rangiradave6@gmail.com>
2023-08-14 20:54:00 +0200

    Does not work

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,12 @@
+/**
+ * init_dog - Function to initialize dog
+ * @d: Pointer to dog structiure
+ * @name: pointer to dog name
+ * @age: dog age
+ * @owner: Pointer to Dog owner
+ * Return: Nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	return (struct dog);
+}
