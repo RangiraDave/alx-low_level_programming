8e8f3ad70dbef6e6463d443dec51922aebbbe037
RangiraDave <rangiradave6@gmail.com>
2023-08-14 13:48:42 +0200

    Dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+TASKS FOR STRUCTURES
+````````````````````
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * dog - Structure name
+ * @name: Pointer to name of dog
+ * @age: Age of a dog
+ * @owner: Name of its owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
