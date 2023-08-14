ca5b0b1db94f9ede22f972fe27371a5f8375d427
RangiraDave <rangiradave6@gmail.com>
2023-08-14 14:28:05 +0200

    Added DOG_H with #ifndef and #endif

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
 /**
- * dog - Structure name
+ * struct dog - Structure name is called dog
  * @name: Pointer to name of dog
  * @age: Age of a dog
  * @owner: Name of its owner
+ *
+ * Description: Structure Dog and its members to use in main function
  */
-
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
