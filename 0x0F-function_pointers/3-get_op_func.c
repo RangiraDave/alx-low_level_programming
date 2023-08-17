b82ea438e3d2ca3b05f9ef5a33d3e61ce19067b6
RangiraDave <rangiradave6@gmail.com>
2023-08-17 09:53:31 +0200

    Replaced for with while in get_op_func

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,12 +19,14 @@ int (*get_op_func(char *s)) (int, int)
 	};
 	int i;
 
-	for (i = 0; i < 5; i++)
+	i = 0;
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 		{
 			return (ops[i].f);
 		}
+		i++;
 	}
 	return (NULL);
 }
