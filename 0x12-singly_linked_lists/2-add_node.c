e433b1a4937ac5f3f28670546f00b58714d6b73b
RangiraDave <rangiradev666@gmail.com>
2023-08-24 22:41:21 +0200

    Function to put a node on hesd of list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - FUnction That Adds a node on head of list
+ * @head: Pointer to the address of head of the list
+ * @str: Pointer to list's str dsts
+ * Return: Address of added node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *Temp = malloc(sizeof(list_t));
+
+	if (Temp == NULL)
+		return (NULL);
+
+	Temp->str = strdup(str);
+	if (Temp->str == NULL)
+	{
+		free(Temp);
+		return (NULL);
+	}
+	Temp->len = strlen(str);
+	Temp->next = *head;
+	*head = Temp;
+	return (Temp);
+}
