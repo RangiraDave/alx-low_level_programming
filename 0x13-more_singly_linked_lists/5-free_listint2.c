26caf9fe53b803f0681648fba07417dc8829f00f
RangiraDave <rangiradev666@gmail.com>
2023-08-28 20:32:53 +0200

    Function to free and add NULL to head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Function that frees list and sets NULL to head
+ * @head: Pointer to current start of the list
+ * Return: Nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	if (*head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	*head = NULL;
+}
