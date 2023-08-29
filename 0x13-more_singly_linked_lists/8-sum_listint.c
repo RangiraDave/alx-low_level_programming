77e7f8a3ece3ab505b8aaf6cd9edd3718231e035
RangiraDave <rangiradev666@gmail.com>
2023-08-29 10:28:31 +0200

    Function to return the sum of nodes in list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function to culculate the sum of all nodes in a list.
+ * @head: Pointer to head node.
+ * Return: Sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	if (head->next == NULL)
+		sum = head->n;
+	while (head->next != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
