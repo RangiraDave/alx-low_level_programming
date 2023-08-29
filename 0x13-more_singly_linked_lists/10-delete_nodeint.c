f0ba6c07e139d3a655563d6b45e7f4d92a4e8730
RangiraDave <rangiradev666@gmail.com>
2023-08-29 15:53:41 +0200

    Added condition for index = 0 and !current

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -27,7 +27,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	while (current)
 	{
-		if (i + 1 == index)
+		if (i == index)
 		{
 			pre->next = current->next;
 			free(current);
