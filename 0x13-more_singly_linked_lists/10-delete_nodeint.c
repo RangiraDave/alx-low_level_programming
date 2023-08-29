6e913f6fbf710c867199f8f40b9c29a56374219e
RangiraDave <rangiradev666@gmail.com>
2023-08-29 15:37:01 +0200

    Function to delete a node at give index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Funtion to delete a node from give index.
+ * @head: Pointer to head node.
+ * @index: Given index in list.
+ * Return: 1 or -1
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *pre, *current;
+	unsigned int i;
+
+	pre = NULL;
+	current = *head;
+	i = 0;
+
+	while (current)
+	{
+		if (i + 1 == index)
+		{
+			pre->next = current->next;
+			free(current);
+			return (1);
+		}
+		pre = current;
+		current = current->next;
+		i++;
+	}
+	return (-1);
+}
