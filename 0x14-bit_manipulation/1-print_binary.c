1f7885aca3d2a3be34893714daa52665af71b838
RangiraDave <rangiradev666@gmail.com>
2023-08-31 21:04:10 +0200

    Adding a function to show binary of unsigned integer

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_binary - Function to print binary numbers for entered decimal.
+ * @n: unsigned decimal to be checked.
+ * Return: Binary generated else 0.
+ */
+
+void print_binary(unsigned int n)
+{
+	if (!n)
+		_putchar('\0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
