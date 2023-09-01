9267c76af2fd789a6fdc8b6c1778a253a3523ee3
RangiraDave <rangiradev666@gmail.com>
2023-09-01 10:38:59 +0200

    ECool Endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -7,5 +7,6 @@ void print_binary(unsigned int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
+int get_endianness(void);
 
 #endif
