cd1169d380b7c4bfccceb43effd9fcf9945ad06d
RangiraDave <rangiradev666@gmail.com>
2023-09-04 20:46:52 +0200

    added error messages

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	if (filename && !text_content)
 	{
-		perror("No text to append");
+		close(fp);
 		return (-1);
 	}
 	fp = open(filename, O_WRONLY | O_APPEND);
