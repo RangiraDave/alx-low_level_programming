b38c52b2074a36aec369ed995fa46322c5d8a09c
RangiraDave <rangiradev666@gmail.com>
2023-09-23 10:05:44 +0200

    Comment issue

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 	{
-		perror("Error openning file");
+/*		perror("Error openning file");*/
 		return (-1);
 	}
 
