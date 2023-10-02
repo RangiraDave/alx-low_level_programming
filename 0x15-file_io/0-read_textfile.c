2a102786025537b32fde2ac46fc916697a3f2293
RangiraDave <rangiradev666@gmail.com>
2023-10-02 11:53:12 +0200

    Read from file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - Function to Read text from a file and print it.
+ * @filename: Pointer to file name.
+ * @letters: Number of characters to be read.
+ * Return: Number of Read characters.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer = malloc(letters + 1);
+	ssize_t bytesRead, bytesWritten;
+
+	if (buffer == NULL)
+		return (0);
+	if (!filename)
+	{
+		free(buffer);
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	bytesRead = read(fd, buffer, letters);
+	if (bytesRead == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	buffer[bytesRead] = '\0';
+	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
+	if (bytesWritten != bytesRead)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (bytesRead);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,5 @@
+This directory contains Tasks for File i/o
+
+Playing with file operations.
+
+Different funtions and programs that open,writes to, appends to, ... a file
