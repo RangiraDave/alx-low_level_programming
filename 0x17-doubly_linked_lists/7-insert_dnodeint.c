1af990ab5bee1944aaf700a5c85e20d0f29f662f
RangiraDave <rangiradev666@gmail.com>
2023-11-09 17:36:28 +0200

    handled -idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp = *h, *new_node = malloc(sizeof(dlistint_t));
 	unsigned int i = 1;
 
-	if (!new_node)
+	if (!new_node || idx < 0)
 		return (NULL);
 
 	if (!h && idx != 0)
