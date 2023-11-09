c51f6f098b23f6807b4f09d9c8dc32af7f04b96c
RangiraDave <rangiradev666@gmail.com>
2023-11-09 16:17:59 +0200

    Handled !head edge

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (!head)
+		return (NULL);
 	if (index == 0)
 		return (head);
 	while (head->next != NULL)
