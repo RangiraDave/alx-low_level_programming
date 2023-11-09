f26bc0e5b0abed76130fb5d29abcc4c160e72488
RangiraDave <rangiradev666@gmail.com>
2023-11-09 10:52:42 +0200

    Function to return nbr of nodes. Added doc in 0-pri..

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - Function to print elements of a list.
+ * @h: Pointer to the head of linked list.
+ * Return: Number of nodes.
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *ptr = h;
---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Function to return number of nodes in a list.
+ * @h: Pointer to head of the list.
+ * Return: Number of elements in list.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t n = 0;
+	if (!h || h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		h = h->next;
+		n++;
+	}
+
+	return (n);
+}
