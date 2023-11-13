b3cc55c22bd93b786547f7bd2a022464435fc320
RangiraDave <rangiradev666@gmail.com>
2023-11-13 12:20:00 +0200

    Betty for main

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 char *_memset(char *s, char b, unsigned int n);
 char *_strcat(char *dest, char *src);
@@ -18,3 +21,5 @@ char *_strstr(char *haystack, char *needle);
 int _abs(int n);
 char *_strcpy(char *dest, char *src);
 int _putchar(char i);
+
+#endif
