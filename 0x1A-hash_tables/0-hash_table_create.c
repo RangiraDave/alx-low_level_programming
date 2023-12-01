8f1de58d63aec437a170b917525ecced50c6ceff
RangiraDave <rangiradev666@gmail.com>
2023-12-01 09:20:01 +0200

    Removed the loop for initialization to NULL on each node as calloc handles that

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,18 +10,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table = malloc(sizeof(hash_table_t));
-	unsigned long int i;
+
+	if (!table)
+		return (NULL);
 
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t));
 
-	if (!table)
-		return (NULL);
 	if (!table->array)
+	{
+		free(table);
 		return (NULL);
-
-	for (i = 0; i < table->size; i++)
-		table->array[i] = NULL;
+	}
 
 	return (table);
 }
