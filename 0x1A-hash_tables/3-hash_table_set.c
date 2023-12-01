c568ae4ac77f378e8922703d5916e7bb196e95c4
RangiraDave <rangiradev666@gmail.com>
2023-12-01 10:54:10 +0200

    added * in malloc

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!key)
 		return (0);
 
-	node = malloc(sizeof(hash_node_t));
+	node = malloc(sizeof(hash_node_t *));
 	if (!node)
 		return (0);
 
