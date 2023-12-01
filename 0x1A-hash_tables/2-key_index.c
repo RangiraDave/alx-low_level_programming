ef23a20984cc63493b86b223342dbc4dde168ab6
RangiraDave <rangiradev666@gmail.com>
2023-12-01 09:06:22 +0200

    Used modulas to retrieve the index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -10,20 +10,8 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	hash_table_t *ht = hash_table_create(size);
 	unsigned long int hash_value = hash_djb2(key);
-	unsigned long int i, index = hash_value % size;
+	unsigned long int index = hash_value % size;
 
-	for (i = 0; i < size; i++)
-	{
-		if (i == index && ht->array[i] != NULL)
-			if (strcmp((const char *)ht->array[i]->key, (const char *)key) == 0)
-		{
-			free(ht);
-			return (i);
-		}
-	}
-
-	free(ht);
-	return (-1);
+	return (index);
 }
