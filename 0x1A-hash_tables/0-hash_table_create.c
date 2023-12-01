c8c50fd4ef1fa6bce05bf879bab335255f1cebf6
RangiraDave <rangiradev666@gmail.com>
2023-12-01 10:56:37 +0200

    Added in malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,13 +9,13 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(size * sizeof(hash_table_t *));
+	hash_table_t *table = malloc(sizeof(hash_table_t));
 
 	if (!table)
 		return (NULL);
 
 	table->size = size;
-	table->array = calloc(table->size, sizeof(hash_node_t));
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 
 	if (!table->array)
 	{
