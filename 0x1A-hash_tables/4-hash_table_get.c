cbf573ecbb8b30551c29938f93aa00b3d768bb99
RangiraDave <rangiradev666@gmail.com>
2023-12-01 11:23:10 +0200

    Added doc

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Function to find a key in @ht.
+ * @ht: Hash table to look in.
+ * @key: The key to look for.
+ * Return: Value associated to the key, Null otherwise.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((unsigned char *)key, ht->size);
+	hash_node_t *node = ht->array[index];
+
+	if (!node || node == NULL)
+		return (NULL);
+
+	if (strcmp(node->key, key) == 0)
+		return (node->value);
+
+	return (NULL);
+}
