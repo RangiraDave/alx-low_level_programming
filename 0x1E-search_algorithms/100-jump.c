f352caf7d42318dca8c0f44e0074965080327cad
RangiraDave <rangiradev666@gmail.com>
2024-03-11 19:55:06 +0200

    space and time complexties

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - Function to implement jump search algorithm.
+ * @array: The array to conduct the search in.
+ * @size: Size of the array.
+ * @value: The element to look for in the array.
+ * Return: The element's index if found, and -1 when size is
+ * equal to zero or the element couln't be found.
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+}
